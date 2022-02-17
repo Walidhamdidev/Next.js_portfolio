44d18d54e3053fbc5c0a70ea66c759efdbe8996b
Walid Hamdi <walidhamdidev@gmail.com>
2022-02-16 17:52:30 -0800

    add skills

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <div className="bg-[#2c3e50a4] bg-opacity-90 p-5 w-full  text-white  flex items-center justify-around text-center">
-      <p>Copyright &copy; {new Date().getFullYear()} Powered by Waleedo</p>
+      <p>Copyright &copy; {new Date().getFullYear()} Powered by Walid Hamdi</p>
     </div>
   );
 }
