bbf8935c77d7387f1139dfcd463257ed087e9d43
Walid Hamdi <walidhamdidev@gmail.com>
2022-03-18 20:52:23 +0100

    about update

--- components/About.js
+++ components/About.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 const transition = { duration: 4, yoyo: Infinity, ease: "easeInOut" };
 
@@ -81,7 +82,7 @@ function About() {
 
       <div className=" relative hidden md:block">
         {/* <div className="h-[437 w-451"></div> */}
-        <svg xmlns="http://www.w3.org/2000/svg" width="451" height="437">
+        {/* <svg xmlns="http://www.w3.org/2000/svg" width="451" height="437">
           <motion.path
             d="M 239 17 C 142 17 48.5 103 48.5 213.5 C 48.5 324 126 408 244 408 C 362 408 412 319 412 213.5 C 412 108 334 68.5 244 68.5 C 154 68.5 102.68 135.079 99 213.5 C 95.32 291.921 157 350 231 345.5 C 305 341 357.5 290 357.5 219.5 C 357.5 149 314 121 244 121 C 174 121 151.5 167 151.5 213.5 C 151.5 260 176 286.5 224.5 286.5 C 273 286.5 296.5 253 296.5 218.5 C 296.5 184 270 177 244 177 C 218 177 197 198 197 218.5 C 197 239 206 250.5 225.5 250.5 C 245 250.5 253 242 253 218.5"
             fill="transparent"
@@ -92,12 +93,20 @@ function About() {
             animate={{ pathLength: 1 }}
             transition={transition}
           />
-        </svg>
-        <motion.div
+        </svg> */}
+        {/* <motion.div
           className="box"
           initial={{ offsetDistance: "0%", scale: 2.5 }}
           animate={{ offsetDistance: "100%", scale: 1 }}
           transition={transition}
+        /> */}
+
+        <Image
+          width={"300%"}
+          height={"450%"}
+          // className="rounded-full"
+          src="/images/hero.svg"
+          alt="Walid Hamdi portfolio photo"
         />
       </div>
     </motion.div>
