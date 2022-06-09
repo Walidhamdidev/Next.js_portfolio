79a98141bc41eefefc7277110c0edcd19a770e8a
Walid Hamdi <walidhamdidev@gmail.com>
2022-06-09 11:18:54 +0100

    change pos

--- components/Navbar.js
+++ components/Navbar.js
@@ -184,7 +184,7 @@ export default function Navbar() {
           <li
             className={` transition duration-700 ease-in-out border-yellow-300 border-opacity-60 lg:border-b-2 hover:text-yellow-500 ${
               // hash === "/#skills" ||
-              scrollPosition >= 1000.2 && scrollPosition < 1200.2
+              scrollPosition >= 1000.2 && scrollPosition < 1280.2
                 ? "text-yellow-500"
                 : ""
             }`}
@@ -196,7 +196,7 @@ export default function Navbar() {
           <li
             className={`  transition duration-700 ease-in-out border-yellow-300 border-opacity-60 lg:border-b-2 hover:text-yellow-500  ${
               // hash === "/#contact" ||
-              scrollPosition >= 1200.2 ? "text-yellow-500" : ""
+              scrollPosition >= 1280.2 ? "text-yellow-500" : ""
             }`}
           >
             <Link href="#contact" passHref>
