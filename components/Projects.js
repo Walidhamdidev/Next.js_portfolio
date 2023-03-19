5dc75750c10f57e7b2f2ae5e968b277ca5f2db93
walid hamdi <walidhamdidev@gmail.com>
2023-03-19 13:02:53 +0100

    update skills & projects

--- components/Projects.js
+++ components/Projects.js
@@ -1,44 +1,44 @@
 import Project from "../components/Project";
 
+const items = [
+  {
+    title: "thenewtunisia.com",
+    url: "https://thenewtunisia.vercel.app/",
+
+    stacks: [
+      { title: "Next.js" },
+      { title: "Peer.js" },
+      { title: "WebRTC" },
+      { title: "Socket.io" },
+      { title: "Context API" },
+      // { title: "D3.js" },
+      { title: "PWA" },
+      { title: "Firebase" },
+      { title: "CSS modules" },
+      { title: "Framer  motion" },
+    ],
+    description:
+      "Empower citizens with a web app integrating multiple sector communication systems, with a voice platform for opinions & insights using ML. Real-time data to decision-makers for a more informed, engaged and responsive society in Tunisia.",
+  },
+  {
+    title: "injestic.com",
+    url: "https://injestic.vercel.app/",
+    stacks: [
+      { title: "Next.js" },
+      { title: "TailwindCSS" },
+      { title: "Context API" },
+      { title: "GraphQL" },
+      { title: "Shopify" },
+      { title: "Framer  motion" },
+    ],
+    description:
+      "E-commerce platform! Designed specifically for Shopify store, this platform seamlessly integrates with an existing store to deliver an enhanced shopping experience to customers.",
+  },
+];
+
 const Projects = () => {
-  const items = [
-    {
-      title: "thenewtunisia.com",
-      url: "https://thenewtunisia.vercel.app/",
-
-      stacks: [
-        { title: "Next.js" },
-        { title: "Peer.js" },
-        { title: "WebRTC" },
-        { title: "Socket.io" },
-        { title: "Context API" },
-        // { title: "D3.js" },
-        { title: "PWA" },
-        { title: "Firebase" },
-        { title: "CSS modules" },
-        { title: "Framer  motion" },
-      ],
-      description:
-        "Empower citizens with a web app integrating multiple sector communication systems, with a voice platform for opinions & insights using ML. Real-time data to decision-makers for a more informed, engaged and responsive society in Tunisia.",
-    },
-    {
-      title: "injestic.com",
-      url: "https://injestic.vercel.app/",
-      stacks: [
-        { title: "Next.js" },
-        { title: "TailwindCSS" },
-        { title: "Context API" },
-        { title: "GraphQL" },
-        { title: "Shopify" },
-        { title: "Framer  motion" },
-      ],
-      description:
-        "E-commerce platform! Designed specifically for Shopify store, this platform seamlessly integrates with an existing store to deliver an enhanced shopping experience to customers.",
-    },
-  ];
-
   return (
-    <div className=" mx-auto lg:px-52 px-5" id="projects">
+    <div className=" mx-auto lg:px-52 px-7" id="projects">
       <h1 className="border-b-4 inline-flex pb-1  text-white  mb-10 font-bold text-3xl md:text-4xl  border-yellow-500">
         Projects
       </h1>
--- components/Skills.js
+++ components/Skills.js
@@ -1,33 +1,32 @@
 import Skill from "../components/Skill";
 
-const Skills = () => {
-  const items = [
-    { skill: "HTML/CSS", type: "foundation" },
-    { skill: "Sass", type: "foundation" },
-    { skill: "JavaScript", type: "foundation" },
-    { skill: "TypeScript", type: "foundation" },
-    { skill: "SQL", type: "foundation" },
-    { skill: "NoSQL", type: "foundation" },
-    { skill: "Git", type: "foundation" },
-    { skill: "Docker", type: "foundation" },
-    { skill: "TailwindCSS", type: "cssFramework" },
-    { skill: "React.js", type: "frontend" },
-    { skill: "Next.js", type: "frontend" },
-    { skill: "Redux", type: "frontend" },
-    { skill: "Node.js", type: "backend" },
-    { skill: "Strapi", type: "backend" },
-    { skill: "Flutter", type: "new" },
-    { skill: "React Native", type: "new" },
-  ];
+const items = [
+  { skill: "HTML/CSS", type: "foundation" },
+  { skill: "Sass", type: "foundation" },
+  { skill: "JavaScript", type: "foundation" },
+  { skill: "TypeScript", type: "foundation" },
+  { skill: "SQL", type: "foundation" },
+  { skill: "NoSQL", type: "foundation" },
+  { skill: "Git", type: "foundation" },
+  { skill: "Docker", type: "foundation" },
+  { skill: "TailwindCSS", type: "cssFramework" },
+  { skill: "React.js", type: "frontend" },
+  { skill: "Next.js", type: "frontend" },
+  { skill: "Redux", type: "frontend" },
+  { skill: "Node.js", type: "backend" },
+  { skill: "Strapi", type: "backend" },
+  { skill: "Flutter", type: "new" },
+  { skill: "React Native", type: "new" },
+];
 
+const Skills = () => {
   return (
     <div className="mt-14  mx-auto lg:px-52 px-5" id="skills">
       <h1 className="border-b-4 inline-flex pb-1 text-white  font-bold text-3xl md:text-4xl  border-yellow-500">
         Technical skills
       </h1>
-      <h3 className=" mb-10">(Build, Test, Deploy)</h3>
 
-      <ul className="">
+      <ul className="mt-7">
         {items.map((item, index) => (
           <Skill key={index} skill={item.skill} skillType={item.type} />
         ))}
