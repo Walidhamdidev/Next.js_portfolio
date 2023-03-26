2d427eb8e0aa5e0760fc8684df8f9bf42160cc3b
Walid Hamdi <56773273+walid-hamdi@users.noreply.github.com>
2023-03-26 15:36:53 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,8 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Walid Hamdi as web developer | flutter | react native | ai "
+            content="Walid Hamdi as software developer, Programming languages (e.g. Java, Python, JavaScript), Development tools (e.g. Git, Visual Studio Code, Eclipse), Frameworks (e.g. Angular, React, Node.js), Software development methodologies (e.g. Agile, Scrum, Waterfall)
+Databases (e.g. MySQL, MongoDB, PostgreSQL), Cloud computing platforms (e.g. AWS, Azure, Google Cloud), DevOps (e.g. Continuous Integration/Continuous Deployment, Infrastructure as Code), Mobile app development (e.g. Android, iOS), Web development (e.g. HTML, CSS, JavaScript), Machine learning (e.g. TensorFlow, PyTorch, scikit-learn)"
           />
         </Head>
         <body className="h-screen w-screen font-body bg-gradient-to-r from-[#2c3e50] to-[#000000] tracking-wider  text-white">
